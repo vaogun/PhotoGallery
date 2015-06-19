11baa63bc235b31b9aa6f613dc96bbd528dfda8f
vaogun <vaogun@gmail.com>
2015-06-19 15:28:25 -0500

    Add "using AsyncTask to run on a background thread" on page 419

--- app/src/main/java/com/vaojr/android/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/vaojr/android/photogallery/PhotoGalleryFragment.java
@@ -1,8 +1,10 @@
 package com.vaojr.android.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,8 @@ import android.widget.GridView;
 import java.io.IOException;
 
 public class PhotoGalleryFragment extends Fragment {
+    private static final String TAG = "PhotoGalleryFragment";
+
     GridView mGridView;
 
     @Override
@@ -18,6 +22,7 @@ public class PhotoGalleryFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
         setRetainInstance(true);
+        new FetchItemsTask().execute();
     }
 
     @Nullable
@@ -29,5 +34,16 @@ public class PhotoGalleryFragment extends Fragment {
         return v;
     }
 
-
+    private class FetchItemsTask extends AsyncTask<Void,Void,Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                String result = new FlickrFetchr().getUrl("http://www.google.com");
+                Log.i(TAG, "Fetched contents of URL: " + result);
+            } catch (IOException ioe) {
+                Log.e(TAG, "Failed to fetch URL: ", ioe);
+            }
+            return null;
+        }
+    }
 }
