fde6d16d9fc7bc3ae9b6051b8ff593879dd85f8c
vaogun <vaogun@gmail.com>
2015-06-20 11:58:00 -0500

    Add "from AsyncTask back to the main thread" on page 429

--- app/src/main/java/com/vaojr/android/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/vaojr/android/photogallery/PhotoGalleryFragment.java
@@ -7,13 +7,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
+import java.util.ArrayList;
 
 public class PhotoGalleryFragment extends Fragment {
     private static final String TAG = "PhotoGalleryFragment";
 
     GridView mGridView;
+    ArrayList<GalleryItem> mItems;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,13 +33,32 @@ public class PhotoGalleryFragment extends Fragment {
 
         mGridView = (GridView)v.findViewById(R.id.gridView);
 
+        setupAdapter();
+
         return v;
     }
-    private class FetchItemsTask extends AsyncTask<Void,Void,Void> {
+
+    void setupAdapter() {
+        if (getActivity() == null || mGridView == null) return;
+
+        if (mItems != null) {
+            mGridView.setAdapter(new ArrayAdapter<GalleryItem>(getActivity(),
+                    android.R.layout.simple_gallery_item, mItems));
+        } else {
+            mGridView.setAdapter(null);
+        }
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void,Void,ArrayList<GalleryItem>> {
         @Override
-        protected Void doInBackground(Void... params) {
-            new FlickrFetchr().fetchItems();
-            return null;
+        protected ArrayList<GalleryItem> doInBackground(Void... params) {
+            return new FlickrFetchr().fetchItems();
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<GalleryItem> items) {
+            mItems = items;
+            setupAdapter();
         }
     }
 }
