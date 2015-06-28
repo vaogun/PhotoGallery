3bd33b1b699967656127de386970a0b8ffcbb6b1
vaogun <vaogun@gmail.com>
2015-06-27 22:06:38 -0500

    Add "looking for new results" on page 471

--- app/src/main/java/com/vaojr/android/photogallery/PollService.java
+++ app/src/main/java/com/vaojr/android/photogallery/PollService.java
@@ -3,8 +3,12 @@ package com.vaojr.android.photogallery;
 import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
+import android.preference.PreferenceManager;
 import android.util.Log;
+
+import java.util.ArrayList;
 
 public class PollService extends IntentService {
     private static final String TAG = "PollService";
@@ -20,6 +24,32 @@ public class PollService extends IntentService {
         boolean isNetworkAvailable = cm.getActiveNetworkInfo() != null;
         if (!isNetworkAvailable) return;
 
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String query = prefs.getString(FlickrFetchr.PREF_SEARCH_QUERY, null);
+        String lastResultId = prefs.getString(FlickrFetchr.PREF_LAST_RESULT_ID, null);
+
         Log.i(TAG, "Received an intent: " + intent);
+
+        ArrayList<GalleryItem> items;
+        if (query != null) {
+            items = new FlickrFetchr().search(query);
+        } else {
+            items = new FlickrFetchr().fetchItems();
+        }
+
+        if(items.size() == 0)
+            return;
+
+        String resultId = items.get(0).getId();
+
+        if (!resultId.equals(lastResultId)) {
+            Log.i(TAG, "Got a new result: " + resultId);
+        } else {
+            Log.i(TAG, "Got an old result: " + resultId);
+        }
+
+        prefs.edit()
+                .putString(FlickrFetchr.PREF_LAST_RESULT_ID, resultId)
+                .commit();
     }
 }
