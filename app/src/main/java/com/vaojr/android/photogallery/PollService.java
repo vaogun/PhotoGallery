49f4a29284fa3ccb2eceda509d54ea6c35c988b4
vaogun <vaogun@gmail.com>
2015-06-28 17:58:24 -0500

    Add "notifications" on page 478

--- app/src/main/java/com/vaojr/android/photogallery/PollService.java
+++ app/src/main/java/com/vaojr/android/photogallery/PollService.java
@@ -2,12 +2,16 @@ package com.vaojr.android.photogallery;
 
 import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.net.ConnectivityManager;
 import android.preference.PreferenceManager;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 
 import java.util.ArrayList;
@@ -15,7 +19,7 @@ import java.util.ArrayList;
 public class PollService extends IntentService {
     private static final String TAG = "PollService";
 
-    private static final int POLL_INTERVAL = 1000 * 15; // 15 seconds
+    private static final int POLL_INTERVAL = 1000 * 60 * 5; // 5 minutes
 
     public PollService() {
         super(TAG);
@@ -51,6 +55,24 @@ public class PollService extends IntentService {
         } else {
             Log.i(TAG, "Got an old result: " + resultId);
         }
+
+        Resources r = getResources();
+        PendingIntent pi = PendingIntent
+                .getActivity(this, 0, new Intent(this, PhotoGalleryActivity.class), 0);
+
+        Notification notification = new NotificationCompat.Builder(this)
+                .setTicker(r.getString(R.string.new_pictures_title))
+                .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                .setContentTitle(r.getString(R.string.new_pictures_title))
+                .setContentText(r.getString(R.string.new_pictures_text))
+                .setContentIntent(pi)
+                .setAutoCancel(true)
+                .build();
+
+        NotificationManager notificationManager = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0,notification);
 
         prefs.edit()
                 .putString(FlickrFetchr.PREF_LAST_RESULT_ID, resultId)
