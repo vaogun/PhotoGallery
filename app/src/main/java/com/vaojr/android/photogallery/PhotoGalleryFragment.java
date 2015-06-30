2f6ed45bc50a28d06b08aaca0bf29d6269c25639
vaogun <vaogun@gmail.com>
2015-06-29 21:00:58 -0500

    Add "dynamic broadcast receivers" on page 490

--- app/src/main/java/com/vaojr/android/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/vaojr/android/photogallery/PhotoGalleryFragment.java
@@ -5,7 +5,6 @@ import android.app.SearchManager;
 import android.app.SearchableInfo;
 import android.content.ComponentName;
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Build;
@@ -28,7 +27,7 @@ import android.widget.ImageView;
 
 import java.util.ArrayList;
 
-public class PhotoGalleryFragment extends Fragment {
+public class PhotoGalleryFragment extends VisibleFragment {
     private static final String TAG = "PhotoGalleryFragment";
 
     GridView mGridView;
