6ee569cffed9daa63d419c818f0151869638825b
vaogun <vaogun@gmail.com>
2015-07-02 20:45:13 -0500

    Add "one last bit of flickr data" on page 501

--- app/src/main/java/com/vaojr/android/photogallery/GalleryItem.java
+++ app/src/main/java/com/vaojr/android/photogallery/GalleryItem.java
@@ -5,6 +5,7 @@ public class GalleryItem {
     private String mCaption;
     private String mId;
     private String mUrl;
+    private String mOwner;
 
     public String toString() {
         return mCaption;
@@ -32,5 +33,17 @@ public class GalleryItem {
 
     public void setUrl(String url) {
         mUrl = url;
+    }
+
+    public String getOwner() {
+        return mOwner;
+    }
+
+    public void setOwner(String owner) {
+        mOwner = owner;
+    }
+
+    public String getPhotoPageUrl() {
+        return "http://www.flickr.com/photos/" + mOwner + "/" + mId;
     }
 }
